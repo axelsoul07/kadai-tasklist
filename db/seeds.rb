bf80f92ba44a190bef73ebaf77d885d806de79ad
axel <saitou9876sai@yahoo.co.jp>
2017-10-18 02:43:09 +0000

    for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 (1..20).each do |number|
-  Task.create(content: 'test content ' + number.to_s, status: 'test status ' + number.to_s)
+  Task.create!(content: 'content ' + number.to_s, status: 'status ' + number.to_s)
 end
